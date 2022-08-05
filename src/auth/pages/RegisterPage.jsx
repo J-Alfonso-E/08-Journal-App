c2b1178f08aeae8f0d857f50c14b41ee22347209
Jorge Alfonso Escutia Izquierdo <alfonso_e_95@hotmail.com>
2022-08-05 12:48:29 -0500

    Front de Crear Cuenta y Login

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,44 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLoayout";
+
 export const RegisterPage = ()=> {
     return (
-        <div>registerPage</div>
+        <AuthLayout tittle='Crear Cuenta'>
+             <form>
+                    <Grid container>
+
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField label="Nombre" type="text" placeholder="Nombre Completo" fullWidth />
+                        </Grid>
+
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField label="Email" type="email" placeholder="correo@prueba.com" fullWidth />
+                        </Grid>
+
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField label="Password" type="password" placeholder="Password" fullWidth />
+                        </Grid>
+                    </Grid>
+
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+
+                        <Grid item xs={12} >
+                            <Button variant="contained" fullWidth>Crear Cuenta</Button>
+                        </Grid>
+
+                        
+
+                        <Grid container direction="row" justifyContent="end">
+                            <Typography sx={{mr : 1}}>Ya tienes cuenta?</Typography>
+                            <Link component={RouterLink} color="inherit" to="/auth/login">
+                            Ingresar
+                            </Link>
+                            
+                        </Grid>
+
+                    </Grid>
+                </form>
+        </AuthLayout>
     )
 }
