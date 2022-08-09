d1ef67a3891bf4016a75d4d29edd21491f04ae2a
Jorge Alfonso Escutia Izquierdo <alfonso_e_95@hotmail.com>
2022-08-09 18:45:51 -0500

    Diseno de Galeria de Imagenes

--- src/Journal/pages/JournalPage.jsx
+++ src/Journal/pages/JournalPage.jsx
@@ -1,12 +1,17 @@
 
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../view";
 
 export const JournalPage = ( ) => {
     return (
         <JournalLayout>
-            {/*<NothingSelectedView />*/}
-            <NoteView />
+            <NothingSelectedView />
+            <IconButton size="large" sx={{color: 'white', backgroundColor: 'error.main', ':hover': {backgroundColor: 'error.main', opacity: 0.9}, position: 'fixed', right: 50, bottom: 50}}>
+                <AddOutlined sx={{fontSize: 30}} />
+            </IconButton>
+            {/*<NoteView />*/}
         </JournalLayout>
         
     )
